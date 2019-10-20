8271f8626ee61b0d709481899db213b4f2e74093
Nikolay Kiselev <ceo@machineand.me>
2019-10-20 19:13:58 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,6 @@ from subprocess import Popen, PIPE
 def test_smoke():
     for command in ['clear', 'init', 'build', 'clear']:
         with Popen(['pipenv', 'run', 'postblog', command], stderr=PIPE) as p:
-            if p.stderr.read():
-                raise Exception('{} failed'.format(command))
+            error = p.stderr.read()
+            if error:
+                raise Exception('{} failed with {}'.format(command, error))
