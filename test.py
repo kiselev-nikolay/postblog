c0cabc9e7b65ebb093fe60ad76c362e52a3049c4
Nikolay Kiselev <ceo@machineand.me>
2019-10-20 19:20:31 +0300

    Updated travis for local smoke tests postblog

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 from subprocess import Popen, PIPE
 
 
-def test_smoke():
-    for command in ['clear', 'init', 'build', 'clear']:
-        with Popen(['pipenv', 'run', 'postblog', command], stderr=PIPE) as p:
-            error = p.stderr.read()
-            if error:
-                raise Exception('{} failed with {}'.format(command, error))
+def test():
+    pass
